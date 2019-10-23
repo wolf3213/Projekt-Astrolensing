6d8bdcdb3a23870cb67b69b28077f838247ace0a
RafalKornel <rafal.kornel123@gmail.com>
2019-10-23 14:59:33 +0200

    Implemented file-opening module, with example on file 'blgXXX_X_i_16.dat', with plotting

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as pl
 from parser import Parser
 
 test_parser = Parser("test")                                        # test object
-test_data = test_parser.read_one_curve("data/blgXXX_X_i_16.dat")    # data from 'blgXXX_X_i_16.dat' file
+test_data = test_parser.read_one_curve("data/blgXXX_X_i_19.dat")    # data from 'blgXXX_X_i_19.dat' file
 
 times = [ a[0] for a in test_data ]                                 # array of times, in julian days
 mags  = [ a[1] for a in test_data ]                                 # array of magnitudos, in mag
