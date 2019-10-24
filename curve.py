e709e60a77ceeae04b3eb0d78de021df80b040fe
RafalKornel <rafal.kornel123@gmail.com>
2019-10-24 11:33:46 +0200

    Some further experimenting

--- curve.py
+++ curve.py
@@ -12,13 +12,17 @@ class Curve:
         self.mags   = np.array([ o[1] for o in filtered ])   # array of magnitudos, in mag
         self.errors = np.array([ o[2] for o in filtered ])   # array of measurement errors
         self.count = len(filtered)
+        self.discarded_value = 0
+
 
         if self.count != 0:
             self.mean_mag = self.mags.mean()
             self.mean_weighted_mag = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
+            self.std_dev = self.mags.std()
         else:
             self.mean_mag = 0
             self.mean_weighted_mag = 0
+            self.std_dev = 0
         
 
         self.data = filtered
@@ -37,15 +41,41 @@ class Curve:
 
         return result
 
+    
+    def discard_three_sig(self):
+        discarded = []
+        _sum = 0
+        #data = self.data
+        for entry in self.data:
+            div = abs(entry[1] - self.mean_mag)
+            #print(f"{self.std_dev} | {div}")
+            if div > 3 * self.std_dev:
+                discarded.append(entry)
+                _sum += div
+
+        #self.update_data(data)
+        self.discarded_value = _sum/(len(discarded)+1)
+        return discarded
+
     def update_data(self, data):
         ''' This function updates object, should 
         be used whenever some entries should be 
         filtered '''
 
-        self.times  = [ o[0] for o in data ]
-        self.mags   = [ o[1] for o in data ]
-        self.errors = [ o[2] for o in data ]
+        self.times  = np.array([ o[0] for o in data ])
+        self.mags   = np.array([ o[1] for o in data ])
+        self.errors = np.array([ o[2] for o in data ])
         self.data = data
+        self.count = len(data)
+
+        if self.count != 0:
+            self.mean_mag = self.mags.mean()
+            self.mean_weighted_mag = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
+            self.std_dev = self.mags.std()
+        else:
+            self.mean_mag = 0
+            self.mean_weighted_mag = 0
+            self.std_dev = 0
 
 
     def plot(self):
@@ -53,8 +83,13 @@ class Curve:
         signle curve. '''
         
         pl.plot(self.times, self.mags, 'o', markersize=0.4)
-        pl.hlines(self.mean_weighted_mag, self.times[0], self.times[-1], \
-            colors='g', label='weighted mean', linewidth=0.5)
+        #pl.hlines(self.mean_weighted_mag, self.times[0], self.times[-1], \
+        #          colors='g', label='weighted mean', linewidth=0.5)
         pl.hlines(self.mean_mag, self.times[0], self.times[-1], \
-            colors='r', label='mean', linewidth=0.5)
+                  colors='r', label='mean', linewidth=0.5)
+        for i in range(1, 4):
+            pl.hlines(self.mean_mag + self.std_dev * i, self.times[0], self.times[-1], \
+                      colors='y', linewidth=0.5 - i/10)
+            pl.hlines(self.mean_mag + self.std_dev * (-i), self.times[0], self.times[-1], \
+                      colors='y', linewidth=0.5 - i/10)
         pl.legend()
--- test.py
+++ test.py
@@ -7,11 +7,24 @@ from curve import Curve
 import os
 
 def test_alg(curve):
-    pass
+    discarded = curve.discard_three_sig()
+    if len(discarded) == 0:
+        return (0, 0)
+    #print(a)
+
+    times  = np.array([ o[0] for o in discarded ])
+    #mags   = np.array([ o[1] for o in discarded ])   
+    #errors = np.array([ o[2] for o in discarded ])
+
+    time_mean = times.mean()
+    time_std  = times.std()
+
+    #print(time_std)
+    return time_std, len(discarded)
 
 error_threshold = 0.1                                               # represents maximum relative error to be included in curve
 
-filename = "data/blgXXX_X_i_1176.dat"
+filename = "data/blgXXX_X_i_2425.dat"
 test_parser = Parser("test")                                        # test object
 test_data = test_parser.read_one_curve(filename)                    # data from @filename file
 test_curve = Curve(test_data, error_threshold)                      # test object holding whole data
@@ -19,22 +32,32 @@ test_curve = Curve(test_data, error_threshold)                      # test objec
 test_curve.plot()
 
 pl.show()
+#pl.savefig("test123.png")
 pl.clf()
+
 
 def plot_all_curves(dir):
     ''' Simple function that plots all curves
     located in 'data/' folder '''
 
-    os.mkdir(f"visualization/{dir}")
+    if dir not in os.listdir("visualization"):
+        os.mkdir(f"visualization/{dir}")
     for filename in os.listdir('data/'):
         parser = Parser("test")                                    
         data = parser.read_one_curve("data/"+filename)
         curve = Curve(data, error_threshold)
 
+
         if curve.count != 0:                                       # aborts plotting if curve has no data
-            curve.plot()
+            std, count = test_alg(curve)
+            if std < std_threshold and count > count_threshold:
+                curve.plot()
+                pl.savefig(f"visualization/{dir}/{filename[:-4:]}.png")
+                pl.clf()
+                print(curve.discarded_value)
 
-        pl.savefig(f"visualization/{dir}/{filename[:-4:]}.png")
-        pl.clf()
 
-plot_all_curves("batch2")
+std_threshold = 290
+count_threshold = 30
+
+#plot_all_curves(f"batch_alg_std{std_threshold}_count{count_threshold}")
