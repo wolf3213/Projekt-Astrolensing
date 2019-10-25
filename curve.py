31f92afc24e2581e3fcc100dccbf23a505f1e27a
RafalKornel <rafal.kornel123@gmail.com>
2019-10-25 21:01:41 +0200

    Found first lens! Refactored code, added weighted time mean in algorithm

--- curve.py
+++ curve.py
@@ -2,30 +2,37 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 class Curve:
-    def __init__(self, data, threshold=0.1):
+    def __init__(self, data, name, threshold=0.1):
         ''' This object is going to be basic data 
             structure. It will contain whole data for one 
             curve, which is one star '''
+
+        self.name = name
 
         filtered = self.filter_nines(data, threshold)
         self.times  = np.array([ o[0] for o in filtered ])   # array of times, in julian days
         self.mags   = np.array([ o[1] for o in filtered ])   # array of magnitudos, in mag
         self.errors = np.array([ o[2] for o in filtered ])   # array of measurement errors
+        self.dist_from_mean = np.zeros(len(self.mags))
         self.count = len(filtered)
-        self.mean_discarded_value = 0
+        self.discarded_mag_mean_value = 0
+        self.time_mean = 0
+        self.time_std = 0
+        self.discarded_count = 0
+        self.mag_mean = 0
+        self.mag_weighted_mean = 0
+        self.mag_std = 0
+        self.mag_max = 0
+        self.mag_min = 0
+        self.some_value = 0
 
         if self.count != 0:
-            self.mean_mag = self.mags.mean()
-            self.mean_weighted_mag = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
-            self.std_dev = self.mags.std()
-            self.max_mag = max(self.mags)
-            self.min_mag = min(self.mags)
-        else:
-            self.mean_mag = 0
-            self.mean_weighted_mag = 0
-            self.std_dev = 0
-            self.max_mag = 0
-            self.min_mag = 0
+            self.mag_mean = self.mags.mean()
+            self.mag_weighted_mean = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
+            self.mag_std = self.mags.std()
+            self.mag_max = max(self.mags)
+            self.mag_min = min(self.mags)
+            
 
         self.data = filtered
         
@@ -52,7 +59,7 @@ class Curve:
         
         result = []
         for entry in self.data:
-            if not entry[2]/self.mean_mag > error_threshold:
+            if not entry[2]/self.mag_mean > error_threshold:
                 result.append(entry)
 
         self.update_data(result)
@@ -66,63 +73,75 @@ class Curve:
         _sum = 0
 
         for entry in self.data:
-            div = entry[1] - self.mean_mag
+            div = entry[1] - self.mag_mean
             
-            if div < -n * self.std_dev:
+            #if abs(div) > n * self.mag_std:
+            if div < -n * self.mag_std:
                 discarded.append(entry)
                 _sum += div
 
-        self.mean_discarded_value = _sum/(len(discarded)+1)
+        self.discarded_mag_mean_value = _sum/(len(discarded)+1)
 
-        return discarded
+        return np.array(discarded)
 
 
     def update_data(self, data):
         ''' This function updates object, should 
-        be used whenever some entries should be 
-        filtered '''
+            be used whenever some entries should be 
+            filtered '''
 
         self.times  = np.array([ o[0] for o in data ])
         self.mags   = np.array([ o[1] for o in data ])
         self.errors = np.array([ o[2] for o in data ])
-        self.data = data
-        self.count = len(data)
+        self.data   = data
+        self.count  = len(data)
 
         if self.count != 0:
-            self.mean_mag = self.mags.mean()
-            self.mean_weighted_mag = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
-            self.std_dev = self.mags.std()
+            self.mag_mean = self.mags.mean()
+            self.mag_weighted_mean = sum(self.mags/(self.errors**2))/sum(1/self.errors**2)
+            self.mag_std = self.mags.std()
+            self.mag_max = max(self.mags)
+            self.mag_min = min(self.mags)
         else:
             self.mean_mag = 0
-            self.mean_weighted_mag = 0
-            self.std_dev = 0
+            self.mag_weighted_mean = 0
+            self.mag_std = 0
+            self.mag_max = 0
+            self.mag_min = 0
+
+        self.dist_from_mean = self.mags - np.full(self.count, self.mag_mean)
+            
 
 
-    def plot(self, mean = True, errors = True, t_min = 0, t_max = 0, t_mean = None):
+    def plot(self, mean = True, errors = False, t_min = 0, t_max = 0, t_mean = None):
         ''' This method is responsible for plotting
-        signle curve. '''
+            single curve. '''
 
         if t_min == 0: t_min = self.times[0]
         if t_max == 0: t_max = self.times[-1]
 
-        height = self.min_mag  
+        height = self.mag_min 
         pl.plot(self.times, self.mags, 'o', markersize=0.4)
 
         if mean:
-            pl.hlines(self.mean_mag, t_min, t_max, \
-                    colors='g', label='mean', linewidth=0.5)
+            pl.hlines(self.mag_mean, t_min, t_max, \
+                    colors='g', label='Mean', linewidth=0.5)
             for i in range(1, 4):
-                pl.hlines(self.mean_mag + self.std_dev * i, t_min, t_max, \
+                pl.hlines(self.mag_mean + self.mag_std * i, t_min, t_max, \
                         colors='y', linewidth=0.5 - i/10)
-                pl.hlines(self.mean_mag + self.std_dev * (-i), t_min, t_max, \
+                pl.hlines(self.mag_mean + self.mag_std * (-i), t_min, t_max, \
                         colors='y', linewidth=0.5 - i/10)
 
         if errors:
-            pl.plot(self.times, self.errors + height, 'o', markersize=0.3, label='errors')
+            pl.plot(self.times, self.errors + height, 'o', markersize=0.3, label='Errors')
             pl.hlines(height, t_min, t_max, colors='r', linewidth=0.5, label='Zero error level')
 
         if t_mean is not None:
-            pl.axvline(x=t_mean, color='r', linewidth='0.4', label='Predicted lens peak')
+            pl.axvline(x=t_mean, color='r', linewidth='0.4', label='Predicted peak')
         
+        pl.xlabel("Time [days]")
+        pl.ylabel("Intensitivity [mag]")
+        pl.title(self.name)
         pl.xlim((t_min, t_max))
+        pl.gca().invert_yaxis()
         pl.legend()
--- test.py
+++ test.py
@@ -4,26 +4,15 @@ import matplotlib.pyplot as pl
 import numpy as np
 from parser import Parser
 from curve import Curve
+from predictor import Predictor
 import os
 
-def test_alg(curve):
-    discarded = curve.discard_n_sig(2)
-    if len(discarded) < 2:
-        return (0, 0, 0)
 
-    times   = np.array([ o[0] for o in discarded ])
-    #mags   = np.array([ o[1] for o in discarded ])   
-    #errors = np.array([ o[2] for o in discarded ])
-
-    time_mean = times.mean()
-    time_std  = times.std()
-
-    return time_mean, time_std, len(discarded)
-
-
-def plot_all_curves(dir):
+def plot_all_curves():
     ''' Simple function that plots all curves
     located in 'data/' folder '''
+
+    dir = f"batch_alg_std{Predictor.std_threshold}_count{Predictor.count_threshold}"
 
     if dir not in os.listdir("visualization"):
         os.mkdir(f"visualization/{dir}")
@@ -32,43 +21,59 @@ def plot_all_curves(dir):
     for filename in os.listdir('data/'):
         parser = Parser("test")                                    
         data = parser.read_one_curve("data/"+filename)
-        curve = Curve(data, error_threshold)
+        curve = Curve(data, filename[5:-4:], Predictor.error_threshold)
 
+        if Predictor.predict_test(curve, Predictor.std_threshold, Predictor.count_threshold):
+            params = f"{curve.some_value:.0}|{curve.time_mean:.1f}|{curve.time_std:.1f}|{curve.discarded_count}|{filename[:-4:]}"
+            
+            curve.plot(t_mean = curve.time_mean)                               # plots whole curve, saves to file in visualization/dir/
+            pl.savefig(f"visualization/{dir}/{filename[:-4:]}.png") 
+            pl.clf()
 
-        ''' this part below should be in separate function '''
-        if curve.count != 0:                                            # aborts plotting if curve has no data
-            mean, std, count = test_alg(curve)
-            if std < std_threshold and count > count_threshold:
+            curve.plot(t_min = curve.time_mean - curve.time_std/2, \
+                        t_max = curve.time_mean + curve.time_std/2, t_mean = curve.time_mean)         # plots only interesting are, saves to file in curves/dir
+            pl.savefig(f"curves/{dir}/{filename[:-4:]}_{params}.png")
+            pl.clf()
 
-                curve.plot(t_mean = mean)                               # plots whole curve, saves to file in visualization/dir/
-                pl.savefig(f"visualization/{dir}/{filename[:-4:]}.png") 
-                pl.clf()
-
-                curve.plot(t_min = mean - std/2, \
-                           t_max = mean + std/2, t_mean = mean)         # plots only interesting are, saves to file in curves/dir
-                pl.savefig(f"curves/{dir}/{filename[:-4:]}.png")
-                pl.clf()
-
-                print(mean, std, count, curve.mean_discarded_value)     # prints parameters
+            #print(curve.time_mean, curve.time_std, curve.discarded_count, curve.some_value)     # prints parameters
+            print(params)
 
 
 if __name__ == "__main__":
-    error_threshold = 0.05                                              # represents maximum relative error to be included in curve
+    Predictor.error_threshold = 0.1                                                # represents maximum relative error to be included in curve
+    Predictor.std_threshold   = 358
+    Predictor.count_threshold = 5
+    Predictor.width           = 3
+    Predictor.value_threshold = 10**0
 
-    filename = "data/photBLG100.1.I.102672.dat"                         # reference lens
-    test_parser = Parser("test")                                        # test object
-    test_data = test_parser.read_one_curve(filename)                    # data from @filename file
-    test_curve = Curve(test_data, error_threshold)                      # test object containing whole data
+    print(f"Relative error threshold: {Predictor.error_threshold}\
+         \n Time standard deviation threshold: {Predictor.std_threshold}\
+         \n Number of discarded points threshold: {Predictor.count_threshold}\
+         \n Number of standard deviations to discard: {Predictor.width}\
+         \n Value threshold: {Predictor.value_threshold}")
 
-    mean, std, count = test_alg(test_curve)
-    print(mean, std, count, test_curve.mean_discarded_value)
+    #filename = "data/photBLG100.1.I.124027.dat"                                # reference lens
+    #filename = "data/photBLG100.1.I.160378.dat"
+    #filename = "data/photBLG100.1.I.119596.dat"
+    #filename = "data/photBLG100.1.I.106173.dat"
+    #filename = "data/photBLG100.1.I.102672.dat"
+    #filename = "data/photBLG100.1.I.101269.dat"
+    filename = "data/blgXXX_X_i_1086.dat"
 
-    test_curve.plot(t_mean=mean)
+    test_parser = Parser("test")                                                # test object
+    test_data = test_parser.read_one_curve(filename)                            # data from @filename file
+    test_curve = Curve(test_data, filename[5:-4:], Predictor.error_threshold)   # test object containing whole data
+    Predictor.predict_test(test_curve)
+
+    params = f"{test_curve.some_value:.0}|{test_curve.time_mean:.1f}|{test_curve.time_std:.1f}|{test_curve.discarded_count}|{filename[:-4:]}"
+    print(params)
+
+    #mean, std, count = test_alg(test_curve)
+    #print(mean, std, count, test_curve.mean_discarded_value)
+
+    test_curve.plot(t_mean=test_curve.time_mean)
 
     pl.show()
     pl.clf()
 
-    std_threshold = 200
-    count_threshold = 4
-
-    plot_all_curves(f"batch_alg_std{std_threshold}_count{count_threshold}")
+    plot_all_curves()
