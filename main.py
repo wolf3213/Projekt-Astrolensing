df17f815b884c0413c8ffcf2ee9cc29d29aaf2ff
wolf3213 <45437300+wolf3213@users.noreply.github.com>
2019-11-16 17:56:51 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,35 +13,40 @@ def plot_all_curves():
     located in 'data/' folder '''
 
     dir = f"batch_alg_std{Predictor.std_threshold}_count{Predictor.count_threshold}"
+    dir = f"victory"
+
+    folder = 'data2009/'#here is name of folder which contains data
 
     if dir not in os.listdir("visualization"):
         os.mkdir(f"visualization/{dir}")
     if dir not in os.listdir("curves"):
         os.mkdir(f"curves/{dir}")
-    for filename in os.listdir('data/'):
+    for filename in os.listdir(folder):
+        #print(filename)
         parser = Parser("test")                                    
-        data = parser.read_one_curve("data/"+filename)
+        data = parser.read_one_curve(folder+filename)
         curve = Curve(data, filename[5:-4:], Predictor.error_threshold)
-
-        if Predictor.predict_test(curve, Predictor.std_threshold, Predictor.count_threshold):
-            params = f"{curve.some_value:.0}|{curve.time_mean:.1f}|{curve.time_std:.1f}|{curve.discarded_count}|{filename[:-4:]}"
+        Predictor.predict_test(curve, Predictor.std_threshold, Predictor.count_threshold)
+        if Predictor.predict_test(curve, Predictor.std_threshold, Predictor.count_threshold):#add here #
+        #if Predictor.predict_test_v2(curve): #add here #
+            params = f"{curve.some_value:.0}|{curve.time_mean:.1f}|{curve.time_std:.1f}|{curve.discarded_count}|{filename[:-4:]}" #add here #
             
             curve.plot(t_mean = curve.time_mean)                               # plots whole curve, saves to file in visualization/dir/
             pl.savefig(f"visualization/{dir}/{filename[:-4:]}.png") 
             pl.clf()
 
-            curve.plot(t_min = curve.time_mean - curve.time_std/2, \
-                        t_max = curve.time_mean + curve.time_std/2, t_mean = curve.time_mean)         # plots only interesting are, saves to file in curves/dir
-            pl.savefig(f"curves/{dir}/{filename[:-4:]}_{params}.png")
-            pl.clf()
+            #curve.plot(t_min = curve.time_mean - curve.time_std/2, \
+            #            t_max = curve.time_mean + curve.time_std/2, t_mean = curve.time_mean)         # plots only interesting are, saves to file in curves/dir
+            #pl.savefig(f"curves/{dir}/{filename[:-4:]}_{params}.png")
+            #pl.clf()
 
             #print(curve.time_mean, curve.time_std, curve.discarded_count, curve.some_value)     # prints parameters
-            print(params)
+            #print(params)
 
 
 if __name__ == "__main__":
     Predictor.error_threshold = 0.1                                                # represents maximum relative error to be included in curve
-    Predictor.std_threshold   = 358
+    Predictor.std_threshold   = 40
     Predictor.count_threshold = 5
     Predictor.width           = 3
     Predictor.value_threshold = 10**0
@@ -58,22 +63,24 @@ if __name__ == "__main__":
     #filename = "data/photBLG100.1.I.106173.dat"
     #filename = "data/photBLG100.1.I.102672.dat"
     #filename = "data/photBLG100.1.I.101269.dat"
-    filename = "data/blgXXX_X_i_1086.dat"
+    #filename = "data/blgXXX_X_i_1086.dat"
+    #filename = "zadanie1/blgXXX_X_i_27155.dat"
+    #filename = "data/blgXXX_X_i_114.dat"
+    #filename="data/1.dat"
+    #test_parser = Parser("test")                                                # test object
+    #test_data = test_parser.read_one_curve(filename)                            # data from @filename file
+    #test_curve = Curve(test_data, filename[5:-4:], Predictor.error_threshold)   # test object containing whole data
+    #Predictor.predict_test(test_curve)
 
-    test_parser = Parser("test")                                                # test object
-    test_data = test_parser.read_one_curve(filename)                            # data from @filename file
-    test_curve = Curve(test_data, filename[5:-4:], Predictor.error_threshold)   # test object containing whole data
-    Predictor.predict_test(test_curve)
-
-    params = f"{test_curve.some_value:.0}|{test_curve.time_mean:.1f}|{test_curve.time_std:.1f}|{test_curve.discarded_count}|{filename[:-4:]}"
-    print(params)
+    #params = f"{test_curve.some_value:.0}|{test_curve.time_mean:.1f}|{test_curve.time_std:.1f}|{test_curve.discarded_count}|{filename[:-4:]}"
+    #print(params)
 
     #mean, std, count = test_alg(test_curve)
     #print(mean, std, count, test_curve.mean_discarded_value)
 
-    test_curve.plot(t_mean=test_curve.time_mean)
+    #test_curve.plot(t_mean=test_curve.time_mean)
 
-    pl.show()
-    pl.clf()
+    #pl.show()
+    #pl.clf()
 
     plot_all_curves()
--- predictor.py
+++ predictor.py
@@ -11,7 +11,8 @@ class Predictor:
     @staticmethod
     def predict_test(curve, std_thr = std_threshold, count_thr = count_threshold):
         #pls add coment here 
-        minimal_count = 1               
+        minimal_count = 1   
+        std_min = 1            
 
         discarded = curve.discard_n_sig(Predictor.width)
         count = len(discarded)
@@ -28,7 +29,7 @@ class Predictor:
         curve.time_std  = times.std()
         curve.discarded_count = count
 
-        found = curve.time_std < std_thr and count > count_thr and curve.some_value > Predictor.value_threshold
+        found = curve.time_std > std_min and curve.time_std < std_thr and count > count_thr and curve.some_value > Predictor.value_threshold
         return found
 
 
@@ -43,3 +44,54 @@ class Predictor:
         times = discarded[:,0]
 
         return sum(times * weights) / sum(weights), sum(weights)
+    
+    @staticmethod
+    def predict_test_v2(curve):
+
+        ''' Idea
+        To be close means disctance beetween two times has to be smaller than close_gap.
+        Then number of close elements is contained in how_much set.
+        It gives positive result when any element of how_much set is bigger than minclose.
+        Numbers of positve results is ,,pos''
+        Ex. 1,2,3,4,5,11,12,13,14,21,30
+        close gap=1.1
+        minclose=3
+        Supposed result
+        How much=[5,4,1,1]
+        pos=3 
+	we can also add if which discards points below medium '''
+        discarded = curve.discard_n_sig(2)
+        times = np.array([o[0] for o in discarded])
+        mags   = np.array([ o[1] for o in discarded ])   
+        how_much=[1] #array containg numbers of close elements in order
+        T=[] #array containg times which are ,,close'' 
+        k=0 #just a variable
+        pos=0
+        close_gap=15.1
+        minclose=7
+        winmags=0
+        for i in range(1, len(times)): # i want iteriate over every element in times 
+            if(times[i]-times[i-1] <= close_gap and mags[i]< curve.mag_mean): #extended function now checks if y-axis also. 
+                k+=1;T.append(times[i])
+            else:
+                how_much.append(k)
+                k=0
+       	for i in range(len(how_much)):   # i want iteriate over every element in how_much
+            if(how_much[i] >= minclose):
+                pos+=1
+        #print(*how_much)
+        for i in range(1, len(mags)):
+            if(mags[i]<=curve.mag_mean-5*curve.mag_std):
+                winmags+=1
+        #print(winmags)
+        #if(pos>=1)
+        print("how_much") 
+        print(*how_much)
+        #print("czasy") 
+        #print(*T) 
+        #print("winmags") 
+        print(winmags)
+        print(pos)  
+        return (pos >= 1 or winmags>=1)  
+
+       
